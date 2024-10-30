7713902104b6ed46bbfcf86094f471df776b423a
luceva2002 <luceva2002@gmail.com>
2024-10-30 08:39:35 +0100

    giggi

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "src/app/components/Projects.jsx";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#060b24]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4"> <br></br>
+        <Hero />
+        <Projects />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
